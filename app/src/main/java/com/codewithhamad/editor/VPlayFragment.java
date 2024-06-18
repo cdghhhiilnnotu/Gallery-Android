30094f42250a70313287ccb6af3d108351091745
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2024-06-18 21:49:19 +0700

    test exoplayer not worked

--- app/src/main/java/com/codewithhamad/editor/VPlayFragment.java
+++ app/src/main/java/com/codewithhamad/editor/VPlayFragment.java
@@ -1,5 +1,6 @@
 package com.codewithhamad.editor;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -16,8 +18,12 @@ import android.widget.VideoView;
 public class VPlayFragment extends Fragment implements IFragment{
 
     VideoView videoView;
+    Button speedBtn;
     GalleryActivity galleryActivity;
     GalleryItem item;
+
+    private final float[] listOfSpeeds = {0.25f, 0.5f, 1.0f, 1.5f, 2.0f};
+    int indexOfSpeed = 2;
 
     public VPlayFragment(GalleryActivity galleryActivity, GalleryItem item){
         this.galleryActivity = galleryActivity;
@@ -32,16 +38,25 @@ public class VPlayFragment extends Fragment implements IFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_v_play, container, false);
 
         videoView = view.findViewById(R.id.video_display);
+        speedBtn = view.findViewById(R.id.video_speed_btn);
         Uri uri = Uri.parse(GalleryConstants.gallery_url + item.item_url);
         videoView.setVideoURI(uri);
         videoView.start();
 
-        MediaController medioController = new MediaController(getContext());
-        videoView.setMediaController(medioController);
+        MediaController mediaController = new MediaController(getContext());
+        videoView.setMediaController(mediaController);
+
+        speedBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                indexOfSpeed++;
+                speedBtn.setText(String.valueOf(listOfSpeeds[indexOfSpeed % 5]) + "x");
+            }
+        });
 
         return view;
     }
